3f93ea30ba6b74a5e23cf906403df7834d8923c9
hugoDuckGame <pingouinafk255@protonmail.com>
2024-07-03 18:09:40 +0200

    removed php keyword in tag

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-aaa
 <head> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -16,7 +15,7 @@ aaa
 <a href="new.php">New PHP</a>
 <a href=""></a>
 
-<?php
+<?
 $counter = 0;
 $unicids = array();
 $page = 0;
