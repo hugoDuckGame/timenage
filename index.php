fb0c60d74b7a8f472f3058d0e587bc0a70a9029e
hugoDuckGame <pingouinafk255@protonmail.com>
2024-07-04 11:37:52 +0200

    added condition to check if user is logged in before displaying all  info on index.php

--- index.php
+++ index.php
@@ -25,77 +25,81 @@ $password = "wV9BuQ-F&t/?V$z";
 $dbname = "dbs13060811";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+if (isset($_COOKIE['sessionID'])) {
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
 
-//First request to retrieve user info
-$sql = "SELECT `f_name` FROM `users` WHERE id='" . $_COOKIE['sessionID'] . "'";
-$result = $conn->query($sql);
+    //First request to retrieve user info
+    $sql = "SELECT `f_name` FROM `users` WHERE id='" . $_COOKIE['sessionID'] . "'";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Bonjour, " . $row['f_name'] . ".";
-  }
-} else {
-  echo "Error 6001 : Unable to log in, please try again";
-}
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Bonjour, " . $row['f_name'] . ".";
+    }
+    } else {
+    echo "Error 6001 : Unable to log in, please try again";
+    }
 
-//Second request to get a list of all the unicids
-$sql = "SELECT `unicid` FROM `usr_projects` WHERE id='" . $_COOKIE['sessionID'] . "' ORDER BY `unicid`";
-$result = $conn->query($sql);
+    //Second request to get a list of all the unicids
+    $sql = "SELECT `unicid` FROM `usr_projects` WHERE id='" . $_COOKIE['sessionID'] . "' ORDER BY `unicid`";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    $unicids[] = $row['unicid'];
-  }
-} else {
-  echo "0 results";
-}
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $unicids[] = $row['unicid'];
+    }
+    } else {
+    echo "0 results";
+    }
 
-print_r($unicids);
+    print_r($unicids);
 
-//Third request to gather the 8 cards
-$sql = "SELECT `unicid`, `proj_name`, `curr_time` FROM `usr_projects` WHERE id='" . $_COOKIE['sessionID'] . "' ORDER BY `unicid`";
-$result = $conn->query($sql);
+    //Third request to gather the 8 cards
+    $sql = "SELECT `unicid`, `proj_name`, `curr_time` FROM `usr_projects` WHERE id='" . $_COOKIE['sessionID'] . "' ORDER BY `unicid`";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  $count = mysqli_num_rows($result);
-  while($row = $result->fetch_assoc()) {
-    echo "<div class='timerBox col-sm-3'>
-            <h5>". $row['proj_name'] ."</h5>
-            <h6>Proj desc</h6>
-            <div class='timer'>
-                <div class='start " . $row['unicid'] . "'>
-                    <button class='btn btn-success' onclick='counter(".  $row['unicid'] . ")'>Start</button>
+    if ($result->num_rows > 0) {
+    // output data of each row
+    $count = mysqli_num_rows($result);
+    while($row = $result->fetch_assoc()) {
+        echo "<div class='timerBox col-sm-3'>
+                <h5>". $row['proj_name'] ."</h5>
+                <h6>Proj desc</h6>
+                <div class='timer'>
+                    <div class='start " . $row['unicid'] . "'>
+                        <button class='btn btn-success' onclick='counter(".  $row['unicid'] . ")'>Start</button>
+                    </div>
+                    <div class='stop " . $row['unicid'] . "'>
+                        <button class='btn btn-danger' onclick='stopCount(" . $row['unicid'] . ")'>Stop</button>
+                    </div>
+                    <timer-tag id='". $row['unicid'] ."'>". gmdate("H:i:s", $row['curr_time']) ."</timer-tag>
                 </div>
-                <div class='stop " . $row['unicid'] . "'>
-                    <button class='btn btn-danger' onclick='stopCount(" . $row['unicid'] . ")'>Stop</button>
-                </div>
-                <timer-tag id='". $row['unicid'] ."'>". gmdate("H:i:s", $row['curr_time']) ."</timer-tag>
             </div>
-        </div>
-        <script>
-            hide('stop')
-        </script>
-        ";
-    if($count>8) {
-        echo '<ul class="pagination">';
-        while ($counter < $count/8) {
-            echo '<li><a href="#"></a></li>';
+            <script>
+                hide('stop')
+            </script>
+            ";
+        if($count>8) {
+            echo '<ul class="pagination">';
+            while ($counter < $count/8) {
+                echo '<li><a href="#"></a></li>';
+            }
         }
     }
-  }
-} else {
-  echo "Error 6001 : Unable to log in, please try again";
+    } else {
+    echo "Error 6001 : Unable to log in, please try again";
+    }
 }
-
+else {
+    echo "<h2>Would you like to log in?</h2><br><a href='login.html'>LOG IN</a>";
+}
 echo "<a href='new.html' class='col-sm-3 newBtn btn btn-info'><span class='glyphicon glyphicon-plus'></span>Add a new task</a>";
 $conn->close();
 
