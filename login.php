cc5db5ab735648b2b0905490e96f658f677e24ae
hugoDuckGame <pingouinafk255@protonmail.com>
2024-07-04 15:24:43 +0200

    fixed login cookie system

--- login.php
+++ login.php
@@ -24,8 +24,14 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 if ($hashed_pass == hash('md5', $_GET['password'])) {
-    setcookie('sessionID', $id, time() + (86400 * 30), "/");
+    if (isset($_COOKIE['sessionID'])){
+      setcookie('sessionID', $id, time() + (86400 * 30), "/");
+    }
+    else{
+      setcookie('sessionID', $id, time() + (86400 * 30));
+    }
     echo "<script>window.location.replace('index.php');</script>";
+    
 }
 else {
     echo "<script>alert('Failed to log in, redirecting'); 
