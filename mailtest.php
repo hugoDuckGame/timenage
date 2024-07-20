a3acdcc34d8a1159fd82a71d2d94ae84d68d7701
hugoDuckGame <pingouinafk255@protonmail.com>
2024-07-20 12:59:19 +0200

    2 times from in From header

--- mailtest.php
+++ mailtest.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
     include 'vars.php';
 
-    $headers = "From: From: contact@duckgame.org" . "\r\n";
+    $headers = "From: Timenage Updates <contact@duckgame.org>" . "\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
     if($_GET['mail'] != "") {
         $mail = $_GET['mail'];
