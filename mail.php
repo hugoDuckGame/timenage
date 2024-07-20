25db0b3b22f580f853d1c680b1cc67296607e715
hugoDuckGame <pingouinafk255@protonmail.com>
2024-07-20 12:17:01 +0200

    fixed mail function and added possibility to put a html file as mail source

--- mail.php
+++ mail.php
@@ -1,14 +1,15 @@
+<meta charset="UTF-8">
 <?php
     include 'vars.php';
 
     $headers = "From: contact@duckgame.com" . "\r\n";
-    if(isset($_GET['mail'])) {
+    if($_GET['mail'] != "") {
         $mail = $_GET['mail'];
+        
     }
     else {
         $mail= file_get_contents($_GET['file']); 
     }
-
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -26,6 +27,7 @@
     // output data of each row
     while($row = $result->fetch_assoc()) {
         mail($row['email'],$_GET['subject'],$mail, $headers);
+        echo $row['email'] . $_GET['subject'] . $mail . $headers;
     }
     } else {
         echo "Error 6001 : Unable to log in, please try again";
